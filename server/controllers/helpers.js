29d6a3c21e081e694def71eb8a5ee9b277479894
Shawn Yates <yates.shawnp@gmail.com>
2019-06-20 22:19:21 -0700

    initial commit for jest setup

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -9,11 +9,10 @@ module.exports = {
     const ownerIds = orgs.map(org => org.ownerUserId);
     const owners = await User.findAll({ where: { uuid: { [opIn]: ownerIds } } });
     return orgs.map((org) => {
-      const { dataValues } = org;
       const orgOwner = owners.find(owner => owner.uuid === org.ownerUserId);
       const { firstName, lastName } = orgOwner;
       const ownerName = `${firstName} ${lastName}`;
-      return { ...dataValues, ownerName };
+      return { ...org, ownerName };
     });
   },
 };
---
+++ server/controllers/test/helpers.jest.spec.js
@@ -0,0 +1,11 @@
+/* global describe, expect, test */
+
+const { teams, owners } = require('./testData');
+
+const { getOrgsWithOwnerName } = require('../helpers');
+
+
+describe('Helpers', () => {
+  describe('getOrgsWithOwnerName', () => {
+  });
+});
