9ef40c4b30bc21a40be1abedd346803bc614c4d2
Shawn Yates <yates.shawnp@gmail.com>
2019-07-14 11:18:55 -0700

    end of container tests

---
+++ client/src/containers/LoggedOutView/test/loggedOutView.spec.jsx
@@ -0,0 +1,32 @@
+/* global describe, expect, test */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import LoggedOutView from '../loggedOutView';
+
+import Login from '../../Login/login';
+import Register from '../../Register/register';
+
+const wrapper = shallow(<LoggedOutView />);
+
+describe('<LoggedOutView />', () => {
+  test('should render a styled div component', () => {
+    const received = wrapper.text();
+    const expected = '<styled.div />';
+    expect(received).toEqual(expected);
+  });
+  test('should render Login component as child if login set to active', () => {
+    wrapper.setState({ isLoginActiveComponent: true });
+    const loginLength = wrapper.find(Login).length;
+    const registerLength = wrapper.find(Register).length;
+    expect(loginLength).toEqual(1);
+    expect(registerLength).toEqual(0);
+  });
+  test('should render Register component as child if login set to inactive', () => {
+    wrapper.setState({ isLoginActiveComponent: false });
+    const loginLength = wrapper.find(Login).length;
+    const registerLength = wrapper.find(Register).length;
+    expect(loginLength).toEqual(0);
+    expect(registerLength).toEqual(1);
+  });
+});
---
+++ client/src/containers/Players/test/players.spec.jsx
@@ -0,0 +1,37 @@
+/* global describe, expect, test */
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import Players from '../players';
+import Table from '../../../components/Table/table';
+
+const mockStore = configureStore([thunk]);
+
+const store = {
+  player: {
+    players: [{ foo: 'bar' }],
+  },
+};
+
+const getWrapper = storeObj => shallow(<Players store={mockStore(storeObj)} />);
+
+describe('<Players />', () => {
+  test('Render a <Players /> component', () => {
+    const received = getWrapper(store).text();
+    const expected = '<Players />';
+    expect(received).toEqual(expected);
+  });
+  test('Renders table as child if Players exist', () => {
+    const deepWrapper = getWrapper(store).dive();
+    const tableLength = deepWrapper.find(Table).length;
+    expect(tableLength).toEqual(1);
+  });
+  test('Does not render table as child if no Players exist', () => {
+    const modifiedStore = { player: { players: null } };
+    const deepWrapper = getWrapper(modifiedStore).dive();
+    const tableLength = deepWrapper.find(Table).length;
+    expect(tableLength).toEqual(0);
+  });
+});
